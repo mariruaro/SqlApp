934fe9a5657784d1d0b010db470bdd7a82495c35
Mariana Viganó <mariana@fdd.com.br>
2018-05-16 15:34:05 -0300

    Final

--- app/src/main/java/br/com/marianarv/sqlapp/MainActivity.java
+++ app/src/main/java/br/com/marianarv/sqlapp/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.marianarv.sqlapp;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,13 +12,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DataBaseHelper myDb;
     EditText editName;
     EditText editLastName;
     EditText editMarks;
+    EditText editId;
     Button addData;
+    Button btnViewAll;
+    Button btnUpdate;
+    Button btnDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +32,59 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         myDb = new DataBaseHelper(this);
 
-        editName = (EditText).findViewById(R.id.editText_Name);
-        editLastName = (EditText).findViewById(R.id.editText_LastName);
-        editMarks = (EditText).findViewById(R.id.editText_Marks);
-        addData = (Button).findViewById(R.id.button_add);
+        editName = (EditText)findViewById(R.id.editText_Name);
+        editLastName = (EditText)findViewById(R.id.editText_LastName);
+        editMarks = (EditText)findViewById(R.id.editText_Marks);
+        editId = (EditText)findViewById(R.id.editText_Id);
+        addData = (Button)findViewById(R.id.button_add);
+        btnViewAll = (Button)findViewById(R.id.button_all);
+        btnUpdate = (Button)findViewById(R.id.button_update);
+        btnDelete = (Button)findViewById(R.id.button_delete);
+        AddData();
+        viewAll();
+        UpdateData();
+        DeleteData();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void DeleteData(){
+        btnDelete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Integer deletedRows = myDb.deleteData(editId.getText().toString());
+                        if(deletedRows>0){
+                            Toast.makeText(MainActivity.this,"Data Deleted",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,"Data not Deleted",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
+    }
+
+    public void UpdateData(){
+        btnUpdate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                       boolean isUpdate = myDb.updateData(editId.getText().toString(),editName.getText().toString(),editLastName.getText().toString(),editMarks.getText().toString());
+                        if(isUpdate){
+                            Toast.makeText(MainActivity.this,"Data Updated",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,"Data not Updated",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
     }
 
     public void AddData(){
@@ -46,11 +93,50 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         boolean isInsert = myDb.insertData(editName.getText().toString(),editLastName.getText().toString(),editMarks.getText().toString());
+                        if(isInsert){
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                        }
                     }
                 }
         );
     }
 
+    public void viewAll(){
+        btnViewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Cursor res = myDb.getAllData();
+                        if(res.getCount()==0){
+                            //show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while(res.moveToNext()){
+                            buffer.append("Id:"+ res.getString(0)+"\n");
+                            buffer.append("Name:"+ res.getString(1)+"\n");
+                            buffer.append("LastName:"+ res.getString(2)+"\n");
+                            buffer.append("Marks:"+ res.getString(3)+"\n\n");
+                        }
+
+                        //show all data
+                        showMessage("Data", buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
