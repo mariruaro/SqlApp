53a54e2eb940aa180bf19a8c2730f3d94a68f2c5
Mariana Viganó <mariana@fdd.com.br>
2018-05-16 15:35:37 -0300

    ajustes

--- app/src/main/java/br/com/marianarv/sqlapp/DataBaseHelper.java
+++ app/src/main/java/br/com/marianarv/sqlapp/DataBaseHelper.java
@@ -1,6 +1,8 @@
 package br.com.marianarv.sqlapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,13 +10,12 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "student.db";
     public static final String TABLE_NAME = "student_table";
     public static final String COL_1 = "ID";
-    public static final String COL_2= "NAME";
+    public static final String COL_2 = "NAME";
     public static final String COL_3 = "LASTNAME";
     public static final String COL_4 = "MARKS";
 
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
     }
 
     @Override
@@ -26,5 +27,41 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS"+TABLE_NAME );
         onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String name,String lastname,String marks){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,lastname);
+        contentValues.put(COL_4,marks);
+        long result = sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor res = sqLiteDatabase.rawQuery("select * from "+TABLE_NAME,null );
+        return res;
+    }
+
+    public boolean updateData(String id,String name,String lastname,String marks){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,lastname);
+        contentValues.put(COL_4,marks);
+        sqLiteDatabase.update(TABLE_NAME, contentValues, "id = ?", new String[]{ id });
+        return true;
+    }
+
+    public Integer deleteData(String id){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        return sqLiteDatabase.delete(TABLE_NAME,"ID = ?",new String[]{id});
     }
 }
